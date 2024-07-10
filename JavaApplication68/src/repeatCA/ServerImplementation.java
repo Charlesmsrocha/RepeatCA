1a09ab385b8c2e455930d53ea5ddb91aa235bd38
Charles Rocha <59806026+Charlesmsrocha@users.noreply.github.com>
2024-07-10 21:59:05 +0100

    Server interface and implementation
    Setting up interface and implementation as it will count integers, but I am still figuring out what I need to do.

--- JavaApplication68/src/repeatCA/ServerImplementation.java
+++ JavaApplication68/src/repeatCA/ServerImplementation.java
@@ -1,12 +1,49 @@
 package repeatCA;
 
-
+import java.rmi.server.UnicastRemoteObject;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author Charles Rocha
  */
 
-public class ServerImplementation  {
+public class ServerImplementation extends UnicastRemoteObject implements ServerInterface {
+    private List<Integer> numList;
+    private int total;
+    private int clientCount;
 
+    protected ServerImplementation() throws RemoteException {
+        numList = new ArrayList<>();
+        total = 0;
+        clientCount = 0;
+    }
+
+    @Override
+    public synchronized void addInteger(int value) throws RemoteException {
+        numList.add(value);
+        total += value;
+    }
+
+    @Override
+    public synchronized int getTotal() throws RemoteException {
+        return total;
+    }
+
+    @Override
+    public synchronized List<Integer> getNumList() throws RemoteException {
+        return numList;
+    }
+
+    @Override
+    public synchronized void incrementClientCount() throws RemoteException {
+        clientCount++;
+    }
+
+    @Override
+    public synchronized int getClientCount() throws RemoteException {
+        return clientCount;
+    }
 }
--- JavaApplication68/src/repeatCA/ServerInterface.java
+++ JavaApplication68/src/repeatCA/ServerInterface.java
@@ -1,12 +1,18 @@
 package repeatCA;
 
-
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
 
 /**
  *
  * @author Charles Rocha
  */
 
-public interface ServerInterface  {
-
+public interface ServerInterface  extends Remote {
+    void addInteger(int value) throws RemoteException;
+    int getTotal() throws RemoteException;
+    List<Integer> getNumList() throws RemoteException;
+    void incrementClientCount() throws RemoteException;
+    int getClientCount() throws RemoteException;
 }
